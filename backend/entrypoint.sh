031d0050c6166b37851d4366cae8da1161d5a722
PedroDutraCarmo <pedroworkdev11@gmail.com>
2026-06-01 20:33:20 -0300

    commit trab

---
+++ backend/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Verificando o banco de dados..."
+
+echo "Aplicando as migrações no banco de dados..."
+python manage.py migrate
+
+echo "Iniciando o servidor Python..."
+exec "$@"
